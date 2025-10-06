5f2cdf7d6f370aa2bab8ca30ecaf5eac908acd90
Zane F. Salti <zfsalti@iastate.edu>
2025-10-06 16:09:52 -0500

    tb

---
+++ staged_mac/tb/new_staged_mac_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.10.2025 16:09:02
+// Design Name: 
+// Module Name: new_staged_mac_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module new_staged_mac_tb(
+
+    );
+endmodule
