454f29f6c9d68be112ea85579de4d176531a4444
Zane F. Salti <zfsalti@iastate.edu>
2025-10-12 13:28:01 -0500

    swift piped mac

---
+++ swift_piped_mac/tb/tcl/piped_mac_tb.tcl
@@ -0,0 +1,148 @@
+relaunch_sim
+
+# Clock and reset
+add_force {/piped_mac/ACLK} -radix hex {0 0ns} {1 5000ps} -repeat_every 10000ps
+add_force {/piped_mac/ARESETN} -radix hex {0 0ns}
+run 30ns
+add_force {/piped_mac/ARESETN} -radix hex {1 0ns}
+# No back pressure
+add_force {/piped_mac/MO_AXIS_TREADY} -radix hex {1 0ns}
+
+
+# 1. Basic Single Operation
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0101 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 50ns
+# Expected:
+#   0x0000_0001
+
+# 1. Basic Single Operation (Underflow)
+#add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TID} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {8111000022220000 0ns}
+#add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+#run 10ns
+#add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+#run 50ns
+# Expected:
+#   0x8000_0000 [Underflow]
+
+# 1. Basic Single Operation (Overflow)
+#add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TID} -radix hex {1 0ns}
+#add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {1111111122222222 0ns}
+#add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+#run 10ns
+#add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+#run 50ns
+# Expected:
+#   0x7FFF_FFFF [Overflow]
+
+
+# 2. Basic Double Operation
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {0 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {2 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0101 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0101 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {0 0ns}
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 50ns
+# Expected:
+#   0x0000_0002
+
+
+# 3. Basic Single Operation back-to-back
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {3 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0203 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {4 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0202 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 50ns
+# Expected:
+#   0x0000_0006
+#   0x0000_0004 ****
+
+
+# 4. Inital Load Single Operation ********
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {0 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {5 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0001 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {1 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {6 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0303 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 50ns
+# Expected:
+#   0x0000_000A *****
+
+
+# 5. Basic Multi Operation
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {0 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {7 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0101 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {8 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0102 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 30ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {9 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0203 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 50 ns
+# Expected:
+#   0x0000_0009
+
+
+# Apply Back Pressure
+add_force {/piped_mac/MO_AXIS_TREADY} -radix hex {1 0ns}
+run 10ns
+
+# 6. Basic Single Operation
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TLAST} -radix hex {1 0ns}
+add_force {/piped_mac/SD_AXIS_TID} -radix hex {A 0ns}
+add_force {/piped_mac/SD_AXIS_TDATA} -radix hex {0204 0ns}
+add_force {/piped_mac/SD_AXIS_TUSER} -radix hex {0 0ns}
+run 10ns
+add_force {/piped_mac/SD_AXIS_TVALID} -radix hex {0 0ns}
+run 30ns
+# Aleaviate Back Pressure
+add_force {/piped_mac/MO_AXIS_TREADY} -radix hex {0 0ns}
+run 50ns
+# Expected:
+#   0x0000_0008
+
